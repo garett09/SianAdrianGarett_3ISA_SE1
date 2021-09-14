5ba830e8d9f2d322e377adf1803511316a292c85
Adrian Sian <adriangarett.sian.iics@ust.edu.ph>
2021-09-14 08:18:09 +0800

    added script1.js

---
+++ js/script1.js
@@ -0,0 +1,13 @@
+function myself(firstname, lastname, age, subject) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+    this.subject = subject;
+    this.age = age;
+    this.introduce = function () {
+        console.log(`Hello I am${this.firstname} ${this.lastname}`)
+    }
+    this.enrolled = function(){
+        console.log(`I am enrolled in ${this.subject}   `)
+    }
+}
