bd3d7097a9eedc43060bb68a46824106d0e27848
Adrian Sian <adriangarett.sian.iics@ust.edu.ph>
2021-09-14 08:27:51 +0800

    added script2.js

---
+++ js/script2.js
@@ -0,0 +1,7 @@
+greet = function(message){
+    if(this.firstname === "Juan" && this.lastname === "Dela Cruz" && message === "Good morning")  {
+        console.log("Good Morning Juan")
+    }else {
+
+    }
+}
