e4032b9618fc9de91854204df5b44232970cc793
Adrian Sian <adriangarett.sian.iics@ust.edu.ph>
2021-09-14 08:57:36 +0800

    added script4.js

---
+++ js/script4.js
@@ -0,0 +1,39 @@
+class Triangle {
+    constructor(side1, side2, side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    setSide1(side1) {
+        this.side1 = side1;
+
+    }
+
+    getSide1() {
+        return this.side1;
+    }
+
+
+    setSide2(side2) {
+        this.side2 = side2;
+
+    }
+
+    getSide2() {
+        return this.side2;
+    }
+
+    setSide3(side3) {
+        this.side3 = side3;
+
+    }
+
+    getSide3() {
+        return this.side3;
+    }
+
+
+    computePerimeter() {
+    }
+}
